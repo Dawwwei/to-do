817a0d8433ed2ecf01c3d5e6cbf1858c092ace8a
Dawei Lin <personal.dawei.lin@gmail.com>
2024-06-22 14:32:44 +0200

    Retouch css, bug fixes and add local storage

--- js/index.js
+++ js/index.js
@@ -1,6 +1,30 @@
+function load() {
+    if (localStorage.getItem('tasks')) {
+        let taskList = document.getElementById('taskList')
+        let tasks = document.getElementsByClassName('taskItem')
+        taskList.innerHTML = localStorage.getItem('tasks');
+        for (let i = 0; i < tasks.length; i++) {
+            tasks[i].addEventListener('click', function () {
+                if (this.classList.contains('completed')) {
+                    this.remove();
+                    hide()
+                }
+                this.classList.add('completed');
+                localStorage.setItem('tasks', taskList.innerHTML)
+            })
+        }
+    }
+    document.getElementById('task').addEventListener('keypress', function (event) {
+        if (event.key === 'Enter') {
+            addTask();
+        }
+    });
+    hide()
+}
+
 function addTask() {
-    let task = document.getElementById('task');
     let taskList = document.getElementById('taskList');
+    let task = document.getElementById('task');
     let taskItem = document.createElement('div');
 
     if (task.value === '') {
@@ -12,19 +36,36 @@ function addTask() {
         taskItem.addEventListener('click', function () {
             if (taskItem.classList.contains('completed')) {
                 taskItem.remove();
+                hide()
             }
             taskItem.classList.add('completed');
         })
         taskList.appendChild(taskItem);
     }
+    localStorage.setItem('tasks', taskList.innerHTML)
+    hide()
 }
 
 function clearTasks() {
+    let taskList = document.getElementById('taskList');
     if (taskList.innerHTML === '') {
         alert('No tasks to clear');
     } else {
-        let taskList = document.getElementById('taskList');
         taskList.innerHTML = '';
-        alert('All tasks cleared')
+        setTimeout(() => {
+            alert('All tasks cleared')
+        }, 100)
+    }
+    localStorage.setItem('tasks', taskList.innerHTML)
+    hide()
+}
+
+function hide() {
+    let taskList = document.getElementById('taskList');
+    let hr = document.getElementById('hr');
+    if (taskList.innerHTML == '') {
+        hr.style.display = 'none';
+    } else {
+        hr.style.display = 'block';
     }
 }
